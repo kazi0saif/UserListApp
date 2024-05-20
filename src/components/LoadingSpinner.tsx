7c601ae3d4bf1fd749881b5497cdcccac5a46ec3
kazi0saif <saifkazi21@gmail.com>
2024-05-21 00:05:10 +0530

    Third commit

--- src/components/LoadingSpinner.tsx
+++ src/components/LoadingSpinner.tsx
@@ -1,8 +1,16 @@
-import React from 'react';
-import './LoadingSpinner.css';
+import React from "react";
+import "./LoadingSpinner.css";
 
 const LoadingSpinner: React.FC = () => {
-  return <div className="spinner"></div>;
+  return (
+    <section>
+      <div className="spinner">
+        <span style={{ "--i": 0 } as React.CSSProperties}></span>
+        <span style={{ "--i": 1 } as React.CSSProperties}></span>
+        <span style={{ "--i": 2 } as React.CSSProperties}></span>
+      </div>
+    </section>
+  );
 };
 
 export default LoadingSpinner;
